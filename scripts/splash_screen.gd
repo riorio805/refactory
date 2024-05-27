f29ba83a8cf86c8a8fd7604457e9d2d2c0e4dedf
riorio805 <sefrianojieftara@gmail.com>
2024-05-27 13:42:17 +0700

    added Splash screen

---
+++ scripts/splash_screen.gd
@@ -0,0 +1,12 @@
+extends Control
+
+var main_game = preload("res://scenes/Main.tscn")
+@onready var anim = $AnimationPlayer
+
+func _ready():
+	anim.play("Splash")
+
+func _process(delta):
+	if not anim.is_playing():
+		get_tree().root.add_child(main_game.instantiate())
+		get_node("/root/Splash").free()
